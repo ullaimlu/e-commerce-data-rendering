0623f60850ab2c51314b94c964b75e5efbcb752b
ullaimlu <marianac1707m@gmail.com>
2024-07-01 14:14:11 -0400

    Create Cart Slice logic

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
+const CartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers:{
+        addItemToCart(state,action){
+            const existingItem =state.cartItems.find(item => item.id === action.payload.id);
+            if (existingItem){
+                existingItem.quantity +=1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity:1});
+            }
+        },
+        removeItemFromCart(state,action){
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+        },
+        clearCart(state){
+            state.cartItems=[];
+        },
+        increaseItemQuanity(state,action){
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease){
+                itemToIncrease.quantity += 1;
+            }
+        },
+        decreaseItemQuatity(state,action){
+            const itemToDecrease =state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease >1){
+                itemToDecrease.quantity -=1;
+            }
+        },
+    },
+    
+});
+
+const initialState =({
+    cartItems:[],
 });
 
 
+export const{
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuanity,
+    decreaseItemQuatity,
+} = CartSlice.actions;
+
+export default CartSlice.reducer;
